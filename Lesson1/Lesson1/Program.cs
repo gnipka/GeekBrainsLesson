bc70db715b2b23ca5be1f3e7a8609e2ab5ea4093
gnipka <gnipka29@gmail.com>
2021-11-01 19:02:57 +0300

    Add files via upload

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToShortDateString()}");
+        }
+    }
+}
