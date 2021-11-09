0774f89a433206281187939a9ecfa66f0278ac4f
gnipka <gnipka29@gmail.com>
2021-11-09 17:20:01 +0300

    Задание 4

---
+++ Lesson_4/Lesson 4+/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Lesson_4_
+{
+    class Program
+    {
+        public static void GetFibonacci(int count, int numFirst, int numSecond)
+        {
+           if(count == 0)
+            {
+                return;
+            };
+            int sum = numFirst + numSecond;
+            count--;
+            numFirst = numSecond;
+            numSecond = sum;
+            Console.Write(numSecond + " ");
+            GetFibonacci(count, numFirst, numSecond);
+            
+        }
+        public static bool IsNum(string str)
+        {
+            int num;
+            bool isNum = int.TryParse(str, out num);
+            if (isNum)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Число введено некорректно!");
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 0, чтобы выйти из программы");
+            while (true)
+            {
+                Console.WriteLine("Сколько вывсести чисел Фибоначчи?");
+                string count = Console.ReadLine();
+                if (count.Trim() == "0")
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    if (IsNum(count))
+                    {
+                        if (Convert.ToInt32(count) == 1)
+                        {
+                            Console.Write("0");
+                        }
+                        else if (Convert.ToInt32(count) == 1)
+                        {
+                            Console.Write("0 1 ");
+                        }
+                        else
+                        {
+                            Console.Write("0 1 ");
+                            GetFibonacci(Convert.ToInt32(count), 0, 1);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Lesson_4/Lesson 4_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson_4_2
+{
+    class Program
+    {
+        public static double ReturnSum(double[] array)
+        {
+            double sum = 0;
+            foreach(var item in array)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+        public static bool IsNum(string str)
+        {
+            double num;
+            bool isNum = double.TryParse(str, out num);
+            if (isNum)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Число введено некорректно!");
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите 00, чтобы выйти из программы");
+                Console.WriteLine("Введите числа");
+                string str = Console.ReadLine();
+                if (str.Trim() == "00")
+                {
+                    Environment.Exit(0);
+                }
+                if (IsNum(str)) 
+                {
+                        var array = str.Split(' ').Select(double.Parse).ToArray();
+                        Console.WriteLine("Сумма чисел: " + ReturnSum(array));
+                }
+            }
+        }
+    }
+}
