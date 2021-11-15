1e3376555fbf3102b1850fc4fc56c30c6f2a5d4a
gnipka <gnipka29@gmail.com>
2021-11-15 20:11:35 +0300

    Задание 5_3

---
+++ Lesson5/Lesson5_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+namespace Lesson5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа от 0 до 255 через пробел");
+            string str = Console.ReadLine();
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+            {
+                // сериализуем весь массив people
+                formatter.Serialize(fs, str);
+
+                Console.WriteLine("Объект сериализован");
+            }
+            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+            {
+                Console.WriteLine((string)formatter.Deserialize(fs));
+            }
+
+        }
+    }
+}
