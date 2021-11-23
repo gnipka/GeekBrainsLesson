e329fd8259991f4c358ca405b6ce58491d846479
gnipka <gnipka29@gmail.com>
2021-11-23 12:18:47 +0300

    Задание7

---
+++ Lesson7Patch/Program.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Lesson7.Program
+// Assembly: Lesson7, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 9FC1546D-0C83-435D-A518-272DA6BCFEB8
+// Assembly location: C:\Users\admin\Desktop\geekbrains\Lesson7Patch\Lesson7Patch.exe
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson7
+{
+  internal class Program
+  {
+    
+  }
+}
